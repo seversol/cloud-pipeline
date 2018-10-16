1493b2c0aa0dce73cf15570b6f48f3e3cdfaf8fa
Scott Eversole <scott.eversole@gmail.com>
2018-10-15 20:18:39 -0500

    Added paging to the findAll API. Sorting to come later.

--- src/main/java/cp/FuelStationController.java
+++ src/main/java/cp/FuelStationController.java
@@ -1,26 +1,32 @@
 package cp;
 
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FuelStationController {
 
-    private final FuelStationRepository repository;
+  private final FuelStationRepository repository;
 
-    FuelStationController(FuelStationRepository repository) {
-        this.repository = repository;
-    }
+  FuelStationController(FuelStationRepository repository) {
+    this.repository = repository;
+  }
 
-    @GetMapping("/api/fuelstations")
-    public Iterable<FuelStation> all() {
-        return repository.findAll();
-    }
+  @GetMapping(value = "/api/fuelstations", params = {"page", "size"})
+  public Iterable<FuelStation> all(
+      @RequestParam(value = "page", defaultValue = "0") int page,
+      @RequestParam(value = "size", defaultValue = "20") int size) {
 
-    @GetMapping("/api/fuelstations/{id}")
-    FuelStation one(@PathVariable Long id) {
+    return repository.findAll(new PageRequest(page, size));
+  }
 
-        return repository.findOne(id);
-    }
+  @GetMapping("/api/fuelstations/{id}")
+  FuelStation one(@PathVariable Long id) {
+
+    return repository.findOne(id);
+  }
 }
